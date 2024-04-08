8b6f5b9a081e76754a3e8a6ba279033eaddbb490
Milan Thapa <tmilan0604@gmail.com>
2024-04-08 17:30:52 +0545

    code update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Expense Dashboard',
+    default: 'Expense Dashboard',
+  },
+  description: 'The Official Dashboard to track expense and income.'
+};
 
 export default function RootLayout({
   children,
